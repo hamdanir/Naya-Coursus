4e468585a52ab24e598df744b2aea4e278bda01c
Hamdan Irfan Wijaya <ifan5570@gmail.com>
2023-02-20 12:18:41 +0700

    update class menggunakan constructor

--- 06-01/oop.js
+++ 06-01/oop.js
@@ -1,32 +1,56 @@
+// class lingkaran {
+
+//     jariJari;
+
+//     luas = () => this.jariJari ** 2 * 3.14;
+//     keliling = () => 3.14 * (this.jariJari + this.jariJari);
+
+// };
+
+// const bunder = new lingkaran();
+// bunder.jariJari = 6;
+// console.log('luas =', bunder.luas());
+// console.log('keliling =', bunder.keliling());
+
+
+// class persegi {
+//     panjang;
+//     lebar;
+
+//     luas = () => this.panjang * this.lebar;
+//     keliling = () => (this.panjang + this.lebar) * 2;
+
+// };
+
+// const adudu = new persegi();
+// adudu.panjang = 2;
+// adudu.lebar = 4;
+// console.log('luas =', adudu.luas());
+// console.log('keliling =', adudu.keliling());
+
+
+// penambahan cunstructor
+
 class lingkaran {
-    // properti
-    jariJari;
-
-
-    // method
+    constructor(jariJari) {
+        this.jariJari = jariJari;
+    }
     luas = () => this.jariJari ** 2 * 3.14;
     keliling = () => 3.14 * (this.jariJari + this.jariJari);
-
 };
-
-const bunder = new lingkaran();
-bunder.jariJari = 6;
-console.log('luas =', bunder.luas());
-console.log('keliling =', bunder.keliling());
-// bunder.keliling;
+const bunder = new lingkaran(3);
+console.log(bunder.luas());
+console.log(bunder.keliling());
 
 class persegi {
-    panjang;
-    lebar;
+    constructor(panjang, lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
 
     luas = () => this.panjang * this.lebar;
     keliling = () => (this.panjang + this.lebar) * 2;
-
 };
-
-const adudu = new persegi();
-adudu.panjang = 2;
-adudu.lebar = 2;
-console.log('luas =', adudu.luas());
-console.log('keliling =', adudu.keliling());
-
+const adudu = new persegi(2, 4);
+console.log(adudu.luas());
+console.log(adudu.keliling());

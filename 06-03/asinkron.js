1ccda352864626bd62f7b00c79faf5a60358f7b7
Hamdan Irfan Wijaya <ifan5570@gmail.com>
2023-02-24 15:58:10 +0700

    update

--- 06-03/asinkron.js
+++ 06-03/asinkron.js
@@ -1,28 +1,50 @@
 const pembagiError = '';
 const inputanError = '';
 
+// const pembagi = (a, b) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (b === 0) {
+//                 const err = new Error('Inputan tidak boleh 0!');
+//                 err.name = pembagiError;
+//                 reject(err);
+//             }
+//             else if (typeof a !== 'number' || typeof b !== 'number') {
+//                 const err = new Error('Inputan harus Angka!!');
+//                 err.name = inputanError;
+//                 reject(err);
+//             }
+//             else resolve(a / b)
+//         }, 2000);
+//     });
+// };
+
 const pembagi = (a, b) => {
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (b === 0) {
+
+        if (b === 0) {
+            setTimeout(() => {
                 const err = new Error('Inputan tidak boleh 0!');
                 err.name = pembagiError;
                 reject(err);
-            }
-            else if (typeof a !== 'number' || typeof b !== 'number') {
+            }, 4000)
+        }
+        else if (typeof a !== 'number' || typeof b !== 'number') {
+            setTimeout(() => {
                 const err = new Error('Inputan harus Angka!!');
                 err.name = inputanError;
                 reject(err);
-            }
-            else resolve(a / b)
-        }, 2000);
+            }, 2000)
+        }
+        else resolve(a / b)
+
     });
 };
 
 
 const main = async () => {
     try {
-        const hasil = await pembagi(3, 7);
+        const hasil = await pembagi(3, 1);
         console.log(hasil);
     } catch (err) {
         console.log(err);
@@ -44,12 +66,12 @@ const cobaAwait = async () => {
     }
 };
 
-cobaAwait();
+// cobaAwait();
 
 const cobaAwait2 = async () => {
     try {
         const hasil = await Promise.race([
-            pembagi(4, 6),
+            pembagi(4 * 2 * 2, 8 + 2 + 5 - 10),
             pembagi(4, 1),
             pembagi(4, 3),
         ]);
@@ -59,9 +81,28 @@ const cobaAwait2 = async () => {
     }
 };
 
-cobaAwait2();
+// cobaAwait2();
 
-// Promise.all([
+const cobaAwait3 = async () => {
+    try {
+        const hasil = await Promise.allSettled([
+            pembagi(4 * 2 * 2, 8 + 2 + 5 - 10),
+            pembagi(4, 2),
+            pembagi(4, 0),
+        ]);
+        hasil.filter(h => h.status == 'fulfilled').forEach(h => {
+            console.log(h.value)
+            console.log(h);
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+// cobaAwait3();
+
+// Promise.allSettled([
 //     pembagi(4, 2),
 //     pembagi(4, 4),
 //     pembagi(4, 1),
